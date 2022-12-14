2e874eba7487d3709b3854b78a782d92fe698b7d
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-14 16:39:38 +0100

    Update lib.h

--- lib.h
+++ lib.h
@@ -1,1 +1,7 @@
-
+#ifndef ESERCITAZIONE_9_LIB_H
+#define ESERCITAZIONE_9_LIB_H
+void riempi(char [10][20]);
+void riempi2(char[20]);
+bool conf(char[10][20], char [10]);
+void analisi(char a[10][20], char b[20]);
+#endif //ESERCITAZIONE_9_LIB_H
