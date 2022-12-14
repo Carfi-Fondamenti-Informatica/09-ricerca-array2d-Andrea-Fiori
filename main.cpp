8a23452f24c60c614d7078790daf011c58cd913a
Andrea-Fiori <115191307+Andrea-Fiori@users.noreply.github.com>
2022-12-14 16:41:29 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <iostream>
-
+#include "lib.h"
+using namespace std;
 int main(){
+    char lista[10][20];
+    char nome[20];
+    riempi(lista);
+    riempi2(nome);
+    conf(lista, nome);
+    analisi(lista, nome);
   return 0;
 }
